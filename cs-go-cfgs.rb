fa891610440102a67e37cabeb411fe636c9a1ff7
Andrew S Aguiar <andrewaguiar6@gmail.com>
2017-12-01 12:40:19 -0200

    adds my cs go configs

---
+++ cs-go-cfgs.rb
@@ -0,0 +1,30 @@
+puts [
+  #Crosshair
+  'cl_crosshair_drawoutline 0',
+  'cl_crosshair_outlinethickness 1',
+  'cl_crosshaircolor 1',
+  'cl_crosshairdot 0',
+  'cl_crosshairgap -2.5',
+  'cl_crosshairsize 3',
+  'cl_crosshairstyle 4',
+  'cl_crosshairthickness 0.5',
+
+  # Granades binds
+  'bind "x" "use weapon_hegrenade"',
+  'bind "c" "use weapon_molotov"',
+  'bind "c" "use weapon_incgrenade"',
+  'bind "v" "use weapon_smokegrenade"',
+  'bind "4" "use weapon_flashbang"',
+
+  # Radar
+  'cl_radar_always_centered 0',
+  'cl_radar_scale 0.3',
+  'cl_hud_radar_scale 1.15',
+
+  # Weapon position
+  'viewmodel_offset_x “0”',
+  'viewmodel_offset_y “-2”',
+  'viewmodel_offset_z “-2”'
+].join(';')
+
+# cl_crosshair_drawoutline 0;cl_crosshair_outlinethickness 1;cl_crosshaircolor 1;cl_crosshairdot 0;cl_crosshairgap -2.5;cl_crosshairsize 3;cl_crosshairstyle 4;cl_crosshairthickness 0.5;bind "x" "use weapon_hegrenade";bind "c" "use weapon_molotov";bind "c" "use weapon_incgrenade";bind "v" "use weapon_smokegrenade";bind "4" "use weapon_flashbang";cl_radar_always_centered 0;cl_radar_scale 0.3;cl_hud_radar_scale 1.15;viewmodel_offset_x “0”;viewmodel_offset_y “-2”;viewmodel_offset_z “-2”
